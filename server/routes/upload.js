b0536722898981995aabe0d434e5619d6199a193
Ivan Felix <ivanfelix.55@gmail.com>
2018-12-03 21:57:07 -0600

    subir imagenes con tipo usuario

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
+const Usuario = require('../models/user');
 
 // default options
 app.use(fileUpload());
 
-app.put('/upload', (req, res) => {
+app.put('/upload/:tipo/:id', (req, res) => {
     if (!req.files) {
         return res.status(400).json({
             ok: false,
             err: {
                 message: 'No hay archivo'
+            }
+        })
+    }
+
+    // Valida tipo
+    let tiposValidos = ['usuarios', 'productos'];
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El tipo no es valido, debe ser: ' + tiposValidos.join(', ')
             }
         })
     }
@@ -31,18 +45,52 @@ app.put('/upload', (req, res) => {
         })
     }
 
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`
+
     // Use the mv() method to place the file somewhere on your server
-    archivo.mv(`uploads/${archivo.name}`, (err) => {
-        if (err)
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
+        if (err) {
             return res.json({
                 ok: false,
                 err
             })
-        res.json({
-            ok: true,
-            message: 'Archivo cargado con exito'
-        })
+        }
+        imagenUsuario(id, res, nombreArchivo);
     });
 })
+
+imagenUsuario = (id, res, nombreArchivo) => {
+    Usuario.findById(id, (err, UsuarioDB) => {
+        if (err) {
+            return res.json({
+                ok: false,
+                err
+            })
+        }
+        if (!UsuarioDB) {
+            return res.json({
+                ok: false,
+                err: {
+                    message: 'El usuario no existe en la base de datos.'
+                }
+            })
+        }
+        UsuarioDB.img = nombreArchivo;
+        UsuarioDB.save((err, usuarioActualizado) => {
+            if (err) {
+                return res.json({
+                    ok: false,
+                    err
+                })
+            }
+            res.json({
+                ok: true,
+                usuarioActualizado,
+                nombreArchivo,
+                message: 'Archivo cargado con exito'
+            })
+        })
+    })
+}
 
 module.exports = app;
